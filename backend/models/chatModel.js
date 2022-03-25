684ee1e7c010ab758c2b2dd09b3f01a726625631
ma-foyez <mafoyez.web@gmail.com>
2022-03-26 02:09:31 +0600

    project add

---
+++ backend/models/chatModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+
+//chatName
+//isGroupChat
+//users
+//latestMessage
+//groupAdmin 
+const chatModel = mongoose.Schema(
+    {
+        chatName: { type: String, trim: true },
+        isGroupChat: { type: Boolean, default: false },
+        users: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ],
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Message"
+        },
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timeStamps: true,
+    }
+)
+
+const Chat = mongoose.model("chat", chatModel);
+
+module.exports = Chat
